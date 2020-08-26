1220ab44a2bb4f777fa904d344f3d8b4b8b3c7f7
gautamtarika <66522643+gautamtarika@users.noreply.github.com>
2020-08-26 19:04:19 +0530

    Create hello_world.c

---
+++ C-Programming_Basic/hello_world.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+
+    return 0;
+}
+
+//" stdio.h " is the header file which needs to be included before writing program. 
+//" printf("") " is used to print the desired output.
