ab5513e55a935eb0b1e6287d2fd7e6d2aaa5724d
gautamtarika <66522643+gautamtarika@users.noreply.github.com>
2020-08-26 19:00:42 +0530

    Add files via upload

---
+++ Python_Basic/arithmeticoperatorsinpython.py
@@ -0,0 +1,19 @@
+a=int(input())
+#here we are reading input by using the "input()" function.
+#"int()" is used to read the input in integer format coz by default input functions read in string type.
+b=int(input())
+print(a+b)
+# this prints sum of a & b
+print(a-b)
+# this prints the difference of a & b i.e. a subtracted from b
+print(a*b)
+# this prints the product of a & b
+print(a/b)
+# this prints the quoteint when a is divided by b.
+print(a**b)
+#this prints a raise to the power b / in other words if u need square of 2 you can use 2**2
+print(a%b)
+# this prints the remainder when a is divide by b
+
+
+#Here We have learn all the arithmetic operators.
