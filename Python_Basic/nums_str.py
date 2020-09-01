0b58c22c3a1ba7ae328988931fcf4020c5c6fed7
Visthruth <visthruthg@gmail.com>
2020-09-01 14:29:01 +0530

    Added numbers and strings in python

---
+++ Python_Basic/nums_str.py
@@ -0,0 +1,33 @@
+#Numbers and strings in Python:
+
+
+
+#numbers in python:
+
+#python accepts the following number formats:
+# int - 1
+# float - 400.01
+# complex - i
+
+# studentId = 375
+
+# student_feedue = 200000.000
+
+
+#strings in python :
+
+str = "HelloWorld!"
+# print(str)         #prints the string on to the screen
+# print(str[0])      #prints the first word in the string
+# print(str[2:8])    #prints the letters 'llowor' on to the screen (here 2 is inclusive and 8 is exclusive)
+# print(str[0:3])    #prints the letters 'Hel' on to the screen.
+
+
+#Question
+#Find out what does it print
+print(str[2:6])
+
+
+
+
+
